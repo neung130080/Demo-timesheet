ddf73ada24a010da8f3f7dfb0571c24984a602d2
Parinya (Trainee) Heochaiyaphum <Parinya_H@vstecs.co.th>
2020-03-15 02:46:09 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,143 @@
+<template>
+  <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item color="purple lighten-2" fill-dot right>
+      <!-- เริ่มกรอกข้อมูลฮีโร่-->
+      <v-card>
+        <v-card-title class="purple lighten-2">
+          <v-icon dark size="42" class="mr-4">mdi-angular</v-icon>
+          <h2 class="display-1 white--text font-weight-light">Hero Tank</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="10"
+            >Accountability: Being the primary method of attack from enemies, focusing on protecting your friends because of their high health.</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team :
+              Is a battle launcher (Should wait for the team to come first)
+              Try to protect And accept attacks from enemies for as long as possible
+              Despite receiving a lot of damage But should not put himself to death in vain
+              Try to keep an eye on the enemy And tell your teammates what the enemy is doing
+            </v-col>
+            <v-col class="hidden-sm-and-down text-right" md="2"></v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item color="amber lighten-1" fill-dot left small>
+      <v-card>
+        <v-card-title class="amber lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Hero Support</h2>
+          <v-icon dark size="42">mdi-needle</v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="8"
+            >Duty: To help friends play games easier, such as Keep an eye on where the enemies are. Heal allies use skills to agitate enemies such as stun, slow, mute.</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team : Help support the fight And support all team members
+              Help friends in the team not to die.
+              Keep an eye on the assassins on the enemy side.
+              Try to keep an eye on the enemy And tell your teammates what the enemy is doing
+            </v-col>
+            <v-icon size="64">mdi-needle</v-icon>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item color="cyan lighten-1" fill-dot right>
+      <v-card>
+        <v-card-title class="cyan lighten-1">
+          <v-icon class="mr-4" dark size="42">mdi-sword</v-icon>
+          <h2 class="display-1 white--text font-weight-light">Hero Fighter</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="8"
+            >Duty: Be the main player in melee damage. Because it has a high life force And high melee attack power (Has a neutral ability between the tank and the assassin)</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team :
+              Try to find the rhythm to attack the main damage of enemies such as damage, Carey and Assassin.
+              Supporting the attack And protect the team
+              Focus on lane control Create a space for friends to walk in the game.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item color="red lighten-1" fill-dot left small>
+      <v-card>
+        <v-card-title class="red lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Hero Assassin</h2>
+          <v-icon dark size="42">mdi-sword-cross</v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col class="hidden-sm-and-down" md="2">
+              <v-icon size="64">mdi-ninja</v-icon>
+            </v-col>
+            <v-col
+              cols="12"
+              md="10"
+            >Duty: Assassinate the enemy. Focusing on agile movement, with severe melee damage But has a low life force</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team :
+              Killing the enemy damage (penetrating)
+              Find an area to dodge enemies. Being in a position that is difficult for the enemy to defend
+              Try to keep moving To prevent being the target of the enemy
+              Watch the rhythm before going into the fight to damage and protect yourself from death.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item color="red lighten-2" fill-dot right>
+      <v-card>
+        <v-card-title class="red lighten-2">
+          <v-icon class="mr-4" dark size="42">mdi-pistol</v-icon>
+          <h2 class="display-1 white--text font-weight-light">Hero Carry</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col>Duty: Be the main player in intense remote physical damage.</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team :
+              Must do the main damage to the team Especially at the end of the game
+              Beware of the melee attacks from the opposing Assassin.
+              Try to survive until the end of the fight.
+              Do not die often because the team will lack damage.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+    <v-timeline-item color="green lighten-1" fill-dot right>
+      <v-card>
+        <v-card-title class="green lighten-1">
+          <v-icon class="mr-4" dark size="42">mdi-humble-bundle</v-icon>
+          <h2 class="display-1 white--text font-weight-light">Hero Mage</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col>Duty: Damage caused by the back line. Has strong magic attack There is a skill to eliminate the enemy rhythm.</v-col>
+            <v-col cols="12" md="10">
+              Fighting style with the team :
+              Standing behind is the main damage in battle (from a distance)
+              Try to use the skills as much as possible, focusing on the enemy Tank and Fighter.
+              The rhythm of the fight to be careful of the opposing side.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+</template>
---
+++ src/views/parasite.vue
@@ -0,0 +1,217 @@
+<template>
+  <v-layout child-flex>
+    <v-btn bottom color="red lighten-2" dark fab fixed right buttom @click="dialog = !dialog">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" width="800px">
+      <v-card>
+        <v-card-title color="white--text" class="orange lighten-5">Save my Rank</v-card-title>
+        <v-container>
+          <v-row class="mx-2">
+            <v-col class="align-center justify-space-between" cols="12">
+              <v-row align="center" class="mr-0">
+                <v-avatar size="40px" class="mx-3">
+                  <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt />
+                </v-avatar>
+                <v-text-field v-model="editedItem.name" placeholder="Name" />
+              </v-row>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field v-model="editedItem.rank" placeholder="Rank" />
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="editedItem.hero" prepend-icon="mdi-account-card-details-outline" placeholder="Hreo"/>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field v-model="editedItem.winrate" placeholder="WIN rate" />
+            </v-col>
+            <v-menu
+              class="orange lighten-3"
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              :return-value.sync="editedItem.date"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="computedDateFormattedMomentjs"
+                  label="Day / month / year"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="editedItem.date" no-title scrollable>
+                <v-spacer></v-spacer>
+                <v-btn text color="red lighten-3" @click="menu = false">Cancel</v-btn>
+                <v-btn color="green accent-1" @click="$refs.menu.save(editedItem.date)">Confirmed</v-btn>
+              </v-date-picker>
+            </v-menu>
+            <v-col cols="12">
+              <v-text-field v-model="editedItem.notes" prepend-icon="mdi-text" placeholder="Notes" />
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-card-actions>
+          <v-btn text color="blue-grey lighten-1">More</v-btn>
+          <v-spacer />
+          <v-btn color="red lighten-3" @click="close">Cancel</v-btn>
+          <v-btn color="green accent-1" @click="save">Confirmed Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat color="indigo lighten-3">
+          <v-toolbar-title class="white--text">Information Level Rank</v-toolbar-title>
+          <v-spacer />
+          <v-text-field
+            flat
+            solo-inverted
+            hide-details
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+            class="hidden-sm-and-down"
+          />
+        </v-toolbar>
+      </template>
+      <template v-slot:item.date="{ item }">{{item.date | formatDate }}</template>
+      <template v-slot:item.action="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="orange lighten-4" @click="initialize">Refresh</v-btn>
+      </template>
+    </v-data-table>
+  </v-layout>
+</template>
+<script>
+import axios from "axios";
+import * as moment from "moment";
+export default {
+  filters: {
+    cuttext: function(val) {
+      if (val && val.length > 60) {
+        return val.substring(0, 60) + "...";
+      } else return val;
+    },
+    formatDate: function(val) {
+      if (val) {
+        return moment(String(val)).format("DD-MMM-YYYY");
+      }
+    }
+  },
+  data: () => ({
+    dialog: false,
+    menu: false,
+    modal: false,
+    menu2: false,
+    headers: [
+      {
+        text: "No.( )",
+        align: "start",
+        sortable: false,
+        value: "id"
+      },
+      { text: "Name", value: "name" },
+      { text: "Rank", value: "rank" },
+      { text: "Hero", value: "hero" },
+      { text: "Win rate", value: "winrate" },
+      { text: "Date", value: "date" },
+      { text: "Notes", value: "notes" },
+      { text: "Actions", value: "action", sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      id: null,
+      name: "",
+      rank: "",
+      hero: "",
+      winrate: "",
+      date: new Date().toISOString(),
+      notes: ""
+    },
+    defaultItem: {
+      id: null,
+      name: "",
+      rank: "",
+      hero: "",
+      winrate: "",
+      date: new Date().toISOString(),
+      notes: ""
+    }
+  }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Save my Rank" : "Edit Item";
+    },
+    computedDateFormattedMomentjs() {
+      return this.editedItem.date
+        ? moment(this.editedItem.date).format("DD-MM-YY")
+        : "";
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize: async function() {
+      let res = await axios({
+        method: "get",
+        url: "http://localhost:3000/mane"
+      });
+      this.desserts = res.data;
+    },
+    editItem(item) {
+      console.log(item);
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    deleteItem: async function(item) {
+      const index = this.desserts.indexOf(item);
+      let xurl = "http://localhost:3000/mane/" + item.id;
+      confirm("Are you sure you want to delete this item?") &&
+        (await axios({ method: "delete", url: xurl }));
+      this.initialize();
+    },
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+    save: async function() {
+      if (this.editedIndex > -1) {
+        let xurl = "http://localhost:3000/mane/" + this.editedItem.id;
+        await axios({
+          method: "put",
+          url: xurl,
+          data: this.editedItem
+        });
+        this.initialize();
+      } else {
+        await axios({
+          method: "post",
+          url: "http://localhost:3000/mane",
+          data: this.editedItem
+        });
+        this.initialize();
+      }
+      this.close();
+    }
+  }
+};
+</script>
